9669393b5d81fa599c345b7c8c838474824cff8a
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-18 12:11:21 +1300

    add: run app migrates

---
+++ Data/DataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Data;
+
+public static class DataExtensions
+{
+    public static void MigrateDb(this WebApplication app)
+    {
+        /*
+            Update DB migrations every time that we run the application.
+        */
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
