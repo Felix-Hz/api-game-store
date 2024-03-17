b3e6c4a6cd3dfe04896ccdbb33ffec53c51f379c
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-18 12:10:36 +1300

    add: gs dependency injection

---
+++ Data/GameStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using GameStore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Data;
+/*
+ DBContext is a combination fo unit of work patterns and repository.
+ Object that creates a session with the DB.
+*/
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    // Objects that need to be mapped.
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    // Modify what runs when applying migrations.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Sports" },
+            new { Id = 3, Name = "Roleplaying" },
+            new { Id = 4, Name = "Family" }
+        );
+    }
+}
